9c6eba30c9018aeca837a05abc5bf5fe4bed2c7e
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-01-11 17:39:31 -0800

    lab03 complete

--- basiclibrary/linter/src/main/java/linter/App.java
+++ basiclibrary/linter/src/main/java/linter/App.java
@@ -3,12 +3,50 @@
  */
 package linter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
+
+
+        Path gatesFile =  Paths.get("src/main/resources/gate.js");
+        missingSemicolon(gatesFile);
+
+    }
+
+    public static String missingSemicolon(Path filePath){
         System.out.println(new App().getGreeting());
+        int line = 1;
+        String errorMessage = "";
+        try {
+            Scanner gatesFileScanner = new Scanner(new File(filePath.toUri()));
+            while(gatesFileScanner.hasNextLine()) {
+                String nextLine = gatesFileScanner.nextLine();
+                if(!nextLine.startsWith("//") && !nextLine.endsWith(";") &&
+                        !nextLine.endsWith("{") && !nextLine.endsWith("}") &&
+                !nextLine.isEmpty() && !nextLine.contains("if") &&
+                        !nextLine.contains("else")) {
+                    errorMessage += String.format("Line %d: Missing semicolon.\n", line);
+                }
+                line++;
+
+            }
+            System.out.println("error message\n" + errorMessage);
+
+        }catch(FileNotFoundException e) {
+            System.out.println(e.getLocalizedMessage());
+            System.out.println("couldn't find the file");
+
+        }
+        return errorMessage;
     }
 }
